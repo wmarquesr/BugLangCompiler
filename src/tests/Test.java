a387743df7a236e14d765ace07568b56fbb82b40
Vecksagos <bsb@ic.ufal.br>
2015-05-25 01:02:47 -0300

    Relative path to files now implamented

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -3,20 +3,19 @@ package tests;
 import java.io.File;
 import java.io.IOException;
 
-import enums.Type;
 import lexicalAnalyzer.LexicalAnalyzer;
-import lexicalAnalyzer.Token;
 
 public class Test {
 
 	public static void main(String[] args) throws IOException {
 		
-		String path = "C:\\Users\\W\\Desktop\\ShellSort.bl";
+		String path = new File("").getCanonicalPath();
+		path+= "/ShellSort.bl";
 		File file = new File(path);
 		
 		LexicalAnalyzer lex = new LexicalAnalyzer(file);
 		
-		while(lex.getAllFileLines().size() != 0){
+		while(lex.size() != 0){
 			lex.nextToken().printToken();
 			
 		}
