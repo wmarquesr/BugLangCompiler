d0b79dd10860e51a87e3a6af303aae9daa09a810
Bruno da Silva Belo <brunodasilvabelo@gmail.com>
2015-09-18 17:03:11 -0300

    teste para o shellsort terminado

--- src/test/java/tests/BuglangTests.java
+++ src/test/java/tests/BuglangTests.java
@@ -1,13 +1,39 @@
 package tests;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.junit.Test;
+
+import lexicalAnalyzer.LexicalAnalyzer;
 
 public class BuglangTests {
 
 	@Test
-	public void testOne() {
-		assertTrue(true);
+	public void testShell() {
+		try {
+			URI source = ClassLoader.getSystemResource("ShellSort.bl").toURI();
+			LexicalAnalyzer la = new LexicalAnalyzer(new File(source));
+			
+			URI sourceRes = ClassLoader.getSystemResource("shellresult").toURI();
+			BufferedReader res = new BufferedReader(new FileReader(sourceRes.getPath()));
+			
+			String line = res.readLine();
+			while (la.size() != 0) {
+				assertEquals(line, la.nextToken().toString());
+				line = res.readLine();
+			}
+			
+			res.close();
+		} catch (URISyntaxException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
