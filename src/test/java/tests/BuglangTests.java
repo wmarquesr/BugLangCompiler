e46dbcef06c5f057cf909dfee7981fb9546c5d5b
Bruno da Silva Belo <brunodasilvabelo@gmail.com>
2015-09-18 15:36:40 -0300

    Começo de tests

---
+++ src/test/java/tests/BuglangTests.java
@@ -0,0 +1,13 @@
+package tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BuglangTests {
+
+	@Test
+	public void testOne() {
+		assertTrue(true);
+	}
+}
